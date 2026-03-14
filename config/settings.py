94b826fa2b55a53d11ac525d9553be948b4dfb95
Suxrob <shuxratovs279@gmail.com>
2026-03-14 15:05:20 +0500

    upload server v1

--- config/settings.py
+++ config/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pu_1osfxrl#-pj8tb5qp!49^&m^3t-+2*84#60x4txvas$zai+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
